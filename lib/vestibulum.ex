7fbe519f567cda1ce649b7bd6e3181d17e34d3b4
Matt Fehskens <Matt Fehskens>
2017-06-15 09:56:28 -0400

    Initial commit

---
+++ lib/vestibulum.ex
@@ -0,0 +1,18 @@
+defmodule Vestibulum do
+  @moduledoc """
+  Documentation for Vestibulum.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vestibulum.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vestibulum_test.exs
@@ -0,0 +1,8 @@
+defmodule VestibulumTest do
+  use ExUnit.Case
+  doctest Vestibulum
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
