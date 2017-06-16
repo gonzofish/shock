8a913e7b6f1be06af743e4e76a97c93687d5c6a0
Matt Fehskens <matt.fehskens@gmail.com>
2017-06-15 21:18:08 -0400

    Default styles added, more base html

--- lib/vestibulum/scaffold.ex
+++ lib/vestibulum/scaffold.ex
@@ -8,6 +8,7 @@ defmodule Vestibulum.Scaffold do
   defp _site_yml do
     """
     description: A new site by me, generated with Vestibulum!
+    language: en
     title: My New Site
     url: http://new-site.com
     """
@@ -35,9 +36,58 @@ defmodule Vestibulum.Scaffold do
   end
 
   defp do_generate_default_theme_files do
-    File.write "./themes/default/assets/styles/main.scss", ""
+    File.write "./themes/default/assets/styles/main.scss", _default_style()
+    File.write "./themes/default/layout/base.eex", _default_base()
     File.write "./themes/default/layout/index.eex", ""
     File.write "./themes/default/layout/page.eex", ""
     File.write "./themes/default/layout/post.eex", ""
+  end
+
+  defp _default_base() do
+    """
+    <!DOCTYPE html>
+    <html lang="<%= @language %>">
+        <head>
+            <title><%= @title %></title>
+
+            <meta name="charset" content="utf8">
+            <meta name="description" content="<%= @description %>">
+
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+            <%= @styles %>
+        </head>
+
+        <body>
+            <div class="container">
+              <h1><%= @title %></h1>
+              <%= @content %>
+            </div>
+
+            <%= @scripts %>
+        </body>
+    </html>
+    """
+  end
+
+  defp _default_style do
+    """
+    body {
+      color: black;
+      font: 14px 'Open Sans', sans-serif;
+    }
+    div.container {
+      align-content: flex-start;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      max-width: 800px;
+      margin: 0 auto;
+      width: 100%;
+
+      h1 {
+        font-size: 300%;
+      }
+    }
+    """
   end
 end
--- test/init_test.exs
+++ test/init_test.exs
@@ -12,9 +12,10 @@ defmodule InitTests do
     assert File.exists? "./site.yml"
 
     { :ok, contents } = File.read "./site.yml"
-    assert contents ===
+    assert contents ==
     """
     description: A new site by me, generated with Vestibulum!
+    language: en
     title: My New Site
     url: http://new-site.com
     """
@@ -33,6 +34,7 @@ defmodule InitTests do
     )
     template_files = ~w(
       ./themes/default/assets/styles/main.scss
+      ./themes/default/layout/base.eex
       ./themes/default/layout/index.eex
       ./themes/default/layout/page.eex
       ./themes/default/layout/post.eex
@@ -40,5 +42,57 @@ defmodule InitTests do
 
     Enum.each(template_dirs, fn(dir) -> File.dir?(dir) |> assert end)
     Enum.each(template_files, fn(file) -> File.exists?(file) |> assert end)
+  end
+
+  test "should create the default base.eex template" do
+    { :ok, contents } = File.read "./themes/default/layout/base.eex"
+    assert contents ==
+    """
+    <!DOCTYPE html>
+    <html lang="<%= @language %>">
+        <head>
+            <title><%= @title %></title>
+
+            <meta name="charset" content="utf8">
+            <meta name="description" content="<%= @description %>">
+
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+            <%= @styles %>
+        </head>
+
+        <body>
+            <div class="container">
+              <h1><%= @title %></h1>
+              <%= @content %>
+            </div>
+
+            <%= @scripts %>
+        </body>
+    </html>
+    """
+  end
+
+  test "should create the default main.scss" do
+    { :ok, contents } = File.read "./themes/default/assets/styles/main.scss"
+    assert contents ==
+    """
+    body {
+      color: black;
+      font: 14px 'Open Sans', sans-serif;
+    }
+    div.container {
+      align-content: flex-start;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      max-width: 800px;
+      margin: 0 auto;
+      width: 100%;
+
+      h1 {
+        font-size: 300%;
+      }
+    }
+    """
   end
 end
